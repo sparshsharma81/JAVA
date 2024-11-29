85b00a78a1c49cd1382198e8d91bfc72ec990194
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2024-11-29 13:46:57 +0530

    helllo universe..
    to kaisa hai milky way universe

---
+++ hellouniverse.java
@@ -0,0 +1,6 @@
+public class hellouniverse{
+    public static void main(String args[]){
+        System.out.println("to kaise ho milkyway universe");
+        System.out.println("the ln is used so that the next line has space...");
+    }
+}
