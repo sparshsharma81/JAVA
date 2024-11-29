22e89807ed1a5fd01f329512429ed5f544f65206
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2024-11-29 13:31:34 +0530

    hello world
    java hello world

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+    public static void main(String args[]){
+        System.out.println("Hello world");
+        System.out.println("To kaise hai aap log");
+    }
+    
+}
