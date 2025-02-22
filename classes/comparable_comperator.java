7c37ac0d8309a28ed6218c9ff21504d96668d8e8
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2025-02-22 20:11:00 +0530

    comparable & non_comparable

---
+++ classes/comparable_comperator.java
@@ -0,0 +1,35 @@
+class comparable_comperator{
+    class sortByMarks implements Comp
+    public static void main(String args[]){
+        /*
+         comparable and comparator
+         interface needs to implement to compare two user defined objects
+
+         compareTo(-)
+         a<b
+         a=b
+         a>b
+
+         class sortByName implements Comparator<Group_25>
+         @override
+
+         comparable and comparator
+
+
+         using lambda function 
+         Collections.sort(arr,(s1,s2)->Integer.compare(s1.marks,s2.marks));
+         the two objects s1 and s2 are compared on the basis of marks ---no need for inheritance and implementation
+         
+
+         using anonymous class ---the commpiler which does not support java 8 or above---there we can not use this method
+         arr.sort(new Comparator<T>(){
+         @override
+         public int compare(Group_25 s1, Group_25 s2){
+         return s1.name.compareTo(s2.name)};
+         })
+         */
+
+
+    }
+
+}
